e649a3912574b9c594daffac2669b85c88fa8812
Magno Biét <magno.biet@gmail.com>
2013-06-04 12:47:26 -0300

    Added support for Windows and Linux

--- concat.py
+++ concat.py
@@ -1,55 +1,67 @@
 import sublime_plugin
 import sublime
 import re
+import os
 
 
 class ConcatCommand(sublime_plugin.TextCommand):
+
     def run(self, edit):
+
         view = self.view
         window = view.window()
         region = sublime.Region(0, view.size())
 
-        path = '/'.join(self.view.file_name().split('/')[0:-1])
+        path = '/'.join(self.view.file_name().split(os.sep)[0:-1])
+
         full_name = self.view.file_name().split('/')[-1]
         extension = full_name.split('.')[-1]
         file_name = '.'.join(full_name.split('.')[0:-1])
 
         test = '/{0,2}@import url\(\'(.+)\'\);'
         i = self.view.find(test, 0)
+
         if i:
+
             new_view = window.new_file()
             new_view.set_name(file_name + '.cat.' + extension)
             region = sublime.Region(0, view.size())
             content = view.substr(region)
 
             try:
+
                 syntax_file = self.view.settings().get('syntax')
                 new_view.set_syntax_file(syntax_file)
+
             except KeyError:
-                print 'no syntax'
+
+                print 'No syntax'
                 pass
 
             edit = new_view.begin_edit('cat')
             new_view.insert(edit, 0, content)
 
             while new_view.find(test, 0):
+
                 i = new_view.find(test, 0)
                 content = new_view.substr(i)
                 m = re.search(test, content)
 
                 if m:
+
                     included = m.group(1)
 
                     try:
-                        f = open(path + '/' + included)
+                        f = open(''.join([path, os.sep, included]))
                         file_content = f.read()
-                        encoded_content = unicode(file_content, "utf-8")
+                        encoded_content = unicode(file_content, 'utf-8')
                         new_view.replace(edit, i, encoded_content)
                         f.close()
+
                     except IOError:
-                        print 'cannot open', included
+                        print 'Cannot open', included
                         raise
 
             new_view.end_edit(edit)
-            window.run_command("save")
-            window.run_command("build")
+            window.run_command('save')
+            window.run_command('build')
